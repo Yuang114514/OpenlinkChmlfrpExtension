26b80d38ca0692f99224131191442d3cd400700d
鬝豭鶬鶬 <achilles.hero.way@outlook.com>
2026-03-01 18:09:34 +0800

    清理fabric

--- src/main/java/cn/Yuang2714/OpenlinkChmlfrpExtension/StaticFields/URLs.java
+++
@@ -1,8 +0,0 @@
-package cn.Yuang2714.OpenlinkChmlfrpExtension.StaticFields;
-
-public class URLs {
-    public static final String panel = "https://panel.chmlfrp.net/home";
-    public static final String api = "https://cf-v2.uapis.cn/";
-    public static final String ipCheck = "http://ip-api.com/json/";
-    public static final String nodes = "https://panel.chmlfrp.net/tunnel/status";
-}
