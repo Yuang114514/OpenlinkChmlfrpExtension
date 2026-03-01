4acc2f8cfb8c2583b8045487d007c1a93954a79e
鬝豭鶬鶬 <achilles.hero.way@outlook.com>
2026-03-01 20:28:55 +0800

    无缝移植！命令也是！

--- src/client/java/cn/yuang2714/OpenlinkChmlfrpExtension/OpenlinkChmlfrpExtension.java
+++ src/client/java/cn/yuang2714/OpenlinkChmlfrpExtension/OpenlinkChmlfrpExtension.java
@@ -1,6 +1,14 @@
 package cn.Yuang2714.OpenlinkChmlfrpExtension;
 
+import cn.Yuang2714.OpenlinkChmlfrpExtension.Util.FrpcManagement;
+import cn.Yuang2714.OpenlinkChmlfrpExtension.Util.LoggingManagement;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,6 +20,60 @@ public class OpenlinkChmlfrpExtension implements ClientModInitializer {
     public static Preferences PREFERENCES = Preferences.userNodeForPackage(OpenlinkChmlfrpExtension.class);
 	@Override
 	public void onInitializeClient() {
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher,buildContext) -> {
+            dispatcher.register(
+                    ClientCommandManager.literal("oce")
+                            .then(ClientCommandManager.literal("setProxyCreationMaxRetry")
+                                    .executes(context -> {
+                                                context.getSource().sendFeedback(Component.translatable("chat.openlink_chmlfrp_extension.command.config_max_retry.read",
+                                                        OpenlinkChmlfrpExtension.PREFERENCES.getInt("config_max_retry", 5)));
+                                                return 1;
+                                            }
+                                    )
+
+                                    .then(ClientCommandManager.argument("value", IntegerArgumentType.integer(1))
+                                            .executes(
+                                                    context -> {
+                                                        int value = context.getArgument("value", int.class);
+                                                        if (value <= 0) {
+                                                            context.getSource().sendError(Component.literal("Not a correct value"));
+                                                            return 0;
+                                                        }
+                                                        OpenlinkChmlfrpExtension.PREFERENCES.putInt("config_max_retry", value);
+
+                                                        context.getSource().sendFeedback(Component.translatable("chat.openlink_chmlfrp_extension.command.config_max_retry.success", value));
+                                                        return 1;
+                                                    }
+                                            )
+                                    )
+                            )
+
+                            .then(ClientCommandManager.literal("reloadUserInfo")
+                                    .executes(
+                                            context -> {
+                                                try {
+                                                    FrpcManagement.initUserEnv();
+                                                    OpenlinkChmlfrpExtension.PREFERENCES.putBoolean("is_logged_in",
+                                                            LoggingManagement.checkToken(OpenlinkChmlfrpExtension.PREFERENCES.get("token", "InvalidToken")));
+                                                    OpenlinkChmlfrpExtension.PREFERENCES.putBoolean("is_in_china",
+                                                            LoggingManagement.userIsInChina());
+                                                    OpenlinkChmlfrpExtension.PREFERENCES.putBoolean("is_vip",
+                                                            LoggingManagement.userIsVIP(OpenlinkChmlfrpExtension.PREFERENCES.get("token", "InvalidToken")));
+                                                    OpenlinkChmlfrpExtension.PREFERENCES.putBoolean("has_real_named",
+                                                            LoggingManagement.userHasRealnamed(OpenlinkChmlfrpExtension.PREFERENCES.get("token", "InvalidToken")));
+                                                    context.getSource().sendFeedback(Component.translatable("chat.openlink_chmlfrp_extension.command.reload_user_info.success"));
+                                                    return 1;
+                                                } catch (Exception e) {
+                                                    OpenlinkChmlfrpExtension.LOGGER.error("{}", e.toString());
+                                                    context.getSource().sendError(Component.translatable("chat.openlink_chmlfrp_extension.command.reload_user_info.fail"));
+                                                    return 0;
+                                                }
+                                            }
+                                    )
+                            )
+            );
+        });
+
 		LOGGER.info("""
                 
                   ____                       _       _         _        ____   _                  _    ______                 ______           _                       _
