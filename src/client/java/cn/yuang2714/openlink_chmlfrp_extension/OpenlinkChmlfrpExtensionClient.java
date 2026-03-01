e35fba76fe824f34f329c407d62a0cf97a3311e4
鬝豭鶬鶬 <achilles.hero.way@outlook.com>
2026-03-01 18:12:46 +0800

    复制fabric文件

---
+++ src/client/java/cn/yuang2714/openlink_chmlfrp_extension/OpenlinkChmlfrpExtensionClient.java
@@ -0,0 +1,10 @@
+package cn.yuang2714.openlink_chmlfrp_extension;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OpenlinkChmlfrpExtensionClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
