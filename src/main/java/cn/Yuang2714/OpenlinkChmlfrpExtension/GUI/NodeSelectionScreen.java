932999b90ed081d0dba217c1a8c7633efff6e5c9
鬝豭鶬鶬 <achilles.hero.way@outlook.com>
2026-03-18 21:41:45 +0800

    Screen Improvement #1: (half and quarter of) 实时节点选择信息

--- src/main/java/cn/Yuang2714/OpenlinkChmlfrpExtension/GUI/NodeSelectionScreen.java
+++ src/main/java/cn/Yuang2714/OpenlinkChmlfrpExtension/GUI/NodeSelectionScreen.java
@@ -22,9 +22,13 @@ public class NodeSelectionScreen extends Screen {
     private boolean startDelay;
     private int delayed = 0;
     private List<Node> nodeList;
-    private Component NodeDescription = Component.translatable("gui.openlink_chmlfrp_extension.node_selection.node_info","","","","","","");
-    private final int TOTAL_HEIGHT = 118; //20 + 4 + (10 * 7) + 4 + 20
-    private final int TOTAL_WIDTH = 200;
+    private Component nodeDescription_name = Component.translatable("gui.openlink_chmlfrp_extension.node_selection.node_info.name","");
+    private Component nodeDescription_description = Component.translatable("gui.openlink_chmlfrp_extension.node_selection.node_info.description","");
+    private Component nodeDescription_location = Component.translatable("gui.openlink_chmlfrp_extension.node_selection.node_info.location","");
+    private Component nodeDescription_bandwidthUsage = Component.translatable("gui.openlink_chmlfrp_extension.node_selection.node_info.bandwidth_usage","");
+    private Component nodeDescription_cpuUsage = Component.translatable("gui.openlink_chmlfrp_extension.node_selection.node_info.cpu_usage","");
+    private int START_Y;
+    private int START_X;
     //第一行：自动选择按钮+面板按钮+节点输入框，40像素一个单位，单独留空
     //   80-2   4  80-2-2   4  32-2
     //+--------+  +--------+  +----+
@@ -41,24 +45,39 @@ public class NodeSelectionScreen extends Screen {
         super.init();
         nodeList = NodeUtil.genNodeList();
 
+        START_X = width / 2 - 100; //200 /2
+        START_Y = height / 2 - 64; //64 = 20 + 4 + (10 * 7)+5+5 + 4 + 20，再/2得到居中
+
         idBox = new EditBox(
                 font,
-                width / 2 - TOTAL_WIDTH / 2 + 80+80+(4/2) +2,
-                height / 2 - TOTAL_HEIGHT / 2 +2,
-                40 -4,
-                20 -4,
+                START_X + 161, // 80+80+(4/2) +1
+                START_Y + 1,
+                40 -2,
+                20 -2,
                 Component.translatable("text.openlink_chmlfrp_extension.none")
         );
         idBox.setValue(String.valueOf(OpenlinkChmlfrpExtension.PREFERENCES.getInt("last_node", -1)));
         idBox.setFilter(text -> text.matches("-*\\d*"));
+        idBox.setResponder(text -> {
+            int enteredId = Integer.parseInt(text);
+            for (Node entered : nodeList) {
+                if (enteredId != -1 && entered.id == enteredId) {
+                    nodeDescription_name = Component.translatable("gui.openlink_chmlfrp_extension.node_selection.node_info.name", entered.name);
+                    nodeDescription_description = Component.translatable("gui.openlink_chmlfrp_extension.node_selection.node_info.description", entered.description);
+                    nodeDescription_location = Component.translatable("gui.openlink_chmlfrp_extension.node_selection.node_info.location", entered.location);
+                    nodeDescription_bandwidthUsage = Component.translatable("gui.openlink_chmlfrp_extension.node_selection.node_info.bandwidth_usage", String.valueOf(entered.bandwidthUsage));
+                    nodeDescription_cpuUsage = Component.translatable("gui.openlink_chmlfrp_extension.node_selection.node_info.cpu_usage", String.valueOf(entered.cpuUsage));
+                }
+            }
+        });
         addRenderableWidget(idBox);
 
         Button panelButton = Button.builder(
                         Component.translatable("gui.openlink_chmlfrp_extension.node_selection.panel_button_text"),
                         button -> Util.getPlatform().openUri(URLs.nodes))
-                .bounds(width / 2 - TOTAL_WIDTH / 2 + 80+(4/2),
-                        height / 2 - TOTAL_HEIGHT / 2,
-                        80 -2-2,
+                .bounds(START_X + 82, //80+(4/2)
+                        START_Y,
+                        76, //80 -2-2
                         20)
                 .build();
         addRenderableWidget(panelButton);
@@ -66,9 +85,9 @@ public class NodeSelectionScreen extends Screen {
         Button autoSelectButton = Button.builder(
                         Component.translatable("gui.openlink_chmlfrp_extension.node_selection.auto_button_text"),
                         button -> idBox.setValue("-1"))
-                .bounds(width / 2 - TOTAL_WIDTH / 2,
-                        height / 2 - TOTAL_HEIGHT / 2,
-                        80 -2,
+                .bounds(START_X,
+                        START_Y,
+                        78, //80 -2
                         20)
                 .build();
         addRenderableWidget(autoSelectButton);
@@ -105,8 +124,8 @@ public class NodeSelectionScreen extends Screen {
                     doneButton.setMessage(Component.translatable("gui.openlink_chmlfrp_extension.node_selection.fail"));
                     doneButton.active = true;
                 })
-                .bounds(width / 2 - TOTAL_WIDTH / 2,
-                        height / 2 + TOTAL_HEIGHT / 2,
+                .bounds(START_X,
+                        START_Y + 128, //20 + 4 + 80 + 4 + 20
                         200,
                         20)
                 .build();
@@ -140,8 +159,24 @@ public class NodeSelectionScreen extends Screen {
                 height / 2 - 75,
                 0xFFFFFF
         );
-        graphics.fill(width / 2 - TOTAL_WIDTH / 2,
-                height / 2 - TOTAL_HEIGHT / 2 +20+4,
+
+        graphics.drawString(
+                font,
+                Component.translatable("gui.openlink_chmlfrp_extension.node_selection.node_info.title"),
+                START_X + 5,
+                START_Y + 39, //20 + 4 + 5 + 10
+                0xFFFFFF
+        );
+        graphics.drawString(
+                font,
+                nodeDescription_name,
+                START_X + 5,
+                START_Y + 49, //20 + 4 + 5 + (10*2)
+                0xFFFFFF
+        );
+
+        graphics.fill(width / 2 - START_X / 2,
+                height / 2 - START_Y / 2 +20+4,
                 200,
                 70,
                 0x80_00_00_00);
